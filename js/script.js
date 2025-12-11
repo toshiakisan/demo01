372c279b6e9836295f997b50a68e313b46838b73
toshiakisan <kozuma@andkou.jp>
2025-12-11 11:21:15 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -218,7 +218,7 @@ function initMainAnimations() {
             const parallaxImages = document.querySelectorAll(".work-img, .profile-img");
             parallaxImages.forEach((img) => {
                 gsap.to(img, {
-                    x: 100, 
+                    x: 60, 
                     ease: "none",
                     scrollTrigger: {
                         trigger: img,
